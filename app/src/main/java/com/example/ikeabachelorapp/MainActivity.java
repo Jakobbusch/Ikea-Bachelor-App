774e1d32a47e7012a4f721b71ad892053f9b3ce9
Jakob Busch Hansen <jakob-busch@hotmail.com>
2022-04-06 11:40:21 +0200

    Best commit ever

--- app/src/main/java/com/example/ikeabachelorapp/MainActivity.java
+++ app/src/main/java/com/example/ikeabachelorapp/MainActivity.java
@@ -4,15 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
     private AppViewModel viewModel;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView webView = findViewById(R.id.modelWebView);
+        webView.loadUrl("https://irongarden.github.io/ZoomRotate/");
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
         viewModel = new ViewModelProvider(this).get(AppViewModel.class);
 
         viewModel.repository.getProductList();
+
     }
 }
+
